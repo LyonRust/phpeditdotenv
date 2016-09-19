c0047914beebc5c5a2c0a6b062d72753233e895e
Andy <php360@qq.com>
2016-09-19 17:47:30 +0800

    添加Dotenv功能文件 load载入env文件功能开发

---
+++ src/Phpeditdotenv/Dotenv.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Dotenv
+ *
+ * 载入.env文件并修改保存
+ *
+ * @author 技安 <php360@qq.com>
+ * @link http://www.moqifei.com 技安后院
+ * @license MIT
+ * @version 1.0.0
+ */
+namespace Agile\Phpeditdotenv;
+
+class Dotenv
+{
+    public static function load($path, $file = '.env')
+    {
+        // 拼接文件
+        $filePath = rtrim($path, '/') . DIRECTORY_SEPARATOR . $file;
+        if (!is_readable($filePath) || !is_file($filePath)) {
+            throw new \InvalidArgumentException(
+                'The file "' . $filePath . '" not readable or not found'
+            );
+        }
+
+        // 在读取在 Macintosh 电脑中或由其创建的文件时， 如果 PHP 不能正确的识别行结束符，启用运行时配置可选项 auto_detect_line_endings 也许可以解决此问题。
+        // 读取当前auto_detect_line_endings设置
+        $autodetect = ini_get('auto_detect_line_endings');
+        // 开启auto_detect_line_endings
+        ini_set('auto_detect_line_endings', '1');
+        // 把env文件读取到数组中
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        // 还原auto_detect_line_endings
+        ini_set('auto_detect_line_endings', $autodetect);
+
+    }
+}
