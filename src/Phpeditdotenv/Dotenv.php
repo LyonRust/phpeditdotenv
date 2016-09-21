e882d0430bb0977b2e48af131bd3fe60e8451b98
Andy <php360@qq.com>
2016-09-21 10:11:23 +0800

    完善读取env文件功能 增加保存env文件功能

--- src/Phpeditdotenv/Dotenv.php
+++ src/Phpeditdotenv/Dotenv.php
@@ -13,6 +13,14 @@ namespace Agile\Phpeditdotenv;
 
 class Dotenv
 {
+    /**
+     * 读取env文件，保留空行和注释
+     *
+     * @param  string $path 文件路径
+     * @param  string $file 文件名，默认".env"
+     *
+     * @return void
+     */
     public static function load($path, $file = '.env')
     {
         // 拼接文件
@@ -33,5 +41,53 @@ class Dotenv
         // 还原auto_detect_line_endings
         ini_set('auto_detect_line_endings', $autodetect);
 
+        $envData = [];
+        foreach ($lines as $line) {
+            // 注释或者空行
+            if (strpos(trim($line), '#') === 0 || empty($line)) {
+                $envData[] = $line;
+                continue;
+            }
+            // 转化成数组
+            if (strpos($line, '=') !== false) {
+                list($name, $value) = array_map('trim', explode('=', $line, 2));
+                $envData[$name] = $value;
+            }
+        }
+
+        return $envData;
+    }
+
+    /**
+     * 保存数据到env文件中，同时保留空行和注释
+     *
+     * @param  array  $envData 要保存的数据
+     * @param  string $path 文件路径
+     * @param  string $file 文件名，默认".env"
+     *
+     * @return void
+     */
+    public static function save(array $envData, $path, $file = '.env')
+    {
+        // 拼接文件
+        $filePath = rtrim($path, '/') . DIRECTORY_SEPARATOR . $file;
+        if (!is_writable($filePath) || !is_file($filePath)) {
+            throw new \InvalidArgumentException(
+                'The file "' . $filePath . '" not writable or not found'
+            );
+        }
+
+        $env = '';
+        foreach ($envData as $key => $value) {
+            if (is_numeric($key)) {
+                $env .= $value;
+            } else {
+                $env .= $key . '=' . $value;
+            }
+
+            $env .= PHP_EOL;
+        }
+
+        return file_put_contents($filePath, $env);
     }
 }
